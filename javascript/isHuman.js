04358d291da19a1aa8ab189d8459f1fcc4e0dd1f
lukaszsmola <lukasz.smola@agnat.pl>
2016-03-10 15:58:27 +0100

    bezinwazyjna weryfikacja botów

---
+++ javascript/isHuman.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lukaszsmola on 10.03.16.
+ */
+
+
+
+var isHuman = false;
+$(document).ready(function () {
+    $(window).scroll(function (event) {
+        isHuman = true;
+        consol.log(isHuman)
+
+    });
+});
+
+
