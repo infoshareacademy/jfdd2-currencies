5492c946c76dc63ef58e93e6239b57b1661aa973
agnieszkaberka <agnberka@gmail.com>
2016-03-12 20:19:31 +0100

    Poprawiony format wprowadzanych danych input number.

--- Javascript/zajawkaZdjecia.js
+++ Javascript/zajawkaZdjecia.js
@@ -1,20 +1,28 @@
 
-var backgrounds = [
-    'url(images/zajawka-bg2-monopoly.png)',
-    'url(images/zajawka-bg2-banknotes.png)',
-    'url(images/zajawka-bg.png)'
-];
-var currentBackgroundId = 0;
-
-function setBackground(backgroundId) {
-    $('header').css({'background': backgrounds[backgroundId],
-    'background-position': 'bottom'});
-}
-
-setInterval(function () {
-    setBackground(currentBackgroundId);
-    currentBackgroundId++;
-    currentBackgroundId %= backgrounds.length;
 
 
-}, 4000);
+//$(document).ready(function () {
+
+    var backgrounds = [
+        'url(images/zajawka-bg2-monopoly.png)',
+        'url(images/zajawka-bg2-banknotes.png)',
+        'url(images/zajawka-bg.png)'
+    ];
+    var currentBackgroundId = 0;
+
+    function setBackground(backgroundId) {
+        $('header').css({
+            'background': backgrounds[backgroundId],
+            'background-position': 'bottom'
+        });
+    }
+
+    setInterval(function () {
+        setBackground(currentBackgroundId);
+        currentBackgroundId++;
+        currentBackgroundId %= backgrounds.length;
+
+
+    }, 4000);
+
+//});
