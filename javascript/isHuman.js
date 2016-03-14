999e256b826f8e08b9ce26bd110d9f4f40a59c90
lukaszsmola <lukasz.smola@agnat.pl>
2016-03-14 11:03:26 +0100

    poprawa console log

--- javascript/isHuman.js
+++ javascript/isHuman.js
@@ -1,14 +1,10 @@
-/**
- * Created by lukaszsmola on 10.03.16.
- */
-
 
 
 var isHuman = false;
 $(document).ready(function () {
     $(window).scroll(function (event) {
         isHuman = true;
-        consol.log(isHuman);
+        console.log(isHuman)
 
     });
 });
