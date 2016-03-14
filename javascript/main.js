9afb2ea64b5a893f87fe5609680914213391ae0c
Olaf <olaf.czempa@gmail.com>
2016-03-14 10:06:31 +0100

    Poprawa wcięć w kodzie. Zespół ustalił, że obowiązującym formatowaniem będą dwie spacje.

--- javascript/main.js
+++ javascript/main.js
@@ -3,28 +3,28 @@
  */
 
 jQuery(document).ready(function () {
-    var offset = 200;
-    var duration = 500;
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
-    });
+  var offset = 200;
+  var duration = 500;
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery('.back-to-top').fadeIn(duration);
+    } else {
+      jQuery('.back-to-top').fadeOut(duration);
+    }
+  });
 
-    jQuery('.back-to-top').click(function (event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    });
+  jQuery('.back-to-top').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  });
 
-    // ZAPOBIEGANIE WYSYŁANIA PUSTGO FORMULARZA
+  // ZAPOBIEGANIE WYSYŁANIA PUSTGO FORMULARZA
 
-    /*if ($.trim($("#email").val()) === "" && $.trim($(" #telephone").val()) === "" ) {
-        $('#form').submit(function(item) {
-            item.preventDefault();
-            alert('Please fill out one of the fields to submit properly');
-        })
-    }*/
+  /*if ($.trim($("#email").val()) === "" && $.trim($(" #telephone").val()) === "" ) {
+   $('#form').submit(function(item) {
+   item.preventDefault();
+   alert('Please fill out one of the fields to submit properly');
+   })
+   }*/
 });
